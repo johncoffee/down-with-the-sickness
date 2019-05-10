feb2acef295b48d46d946b1ed86e484c1134f8a3
julian <jh@practio.com>
2019-05-10 17:28:56 +0200

    first

---
+++ src/disease.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk');
+
+
+let adjectives = [
+  'super',
+  'shameful',
+  'airborne',
+]
+
+let verbs = [
+  'bleeding',
+  'unique',
+  'aggressive',
+  'self-inflicted',
+]
+
+let specific = [
+  'brain-',
+  'ass-',
+  'stomach-',
+  'throat-and-mouth-',
+]
+
+let nouns = [
+  'cramps',
+  'aids',
+  'polio',
+  'cancer',
+  'hepatitis-X',
+  'infection',
+  'wounds',
+  'battle-scars',
+]
+
+function getRandom (list, chanceOfEmpty = 1,) {
+  return (chanceOfEmpty === 1 || Math.random() <= chanceOfEmpty) ? list[Math.floor(Math.random() * list.length)] : ""
+}
+
+function generateRouteName () {
+  const name = [
+    getRandom(adjectives, 0.75),
+    ' ',
+    getRandom(verbs),
+    ' ',
+    getRandom(specific, 0.33),
+    getRandom(nouns),
+  ].filter(v => !!v)
+    .filter((v, idx, arr) => arr[idx-1] !== v)
+    .map(str => str.charAt(0).toUpperCase() + str.slice(1))
+    .join('')
+    .trim()
+
+  return name
+}
+
+
+if (!module.parent) {
+  const name = generateRouteName()
+  console.log(chalk.red(`  ${name}`))
+}
+
+module.exports = {
+  generateRouteName
+}
---
+++ src/treatment.js
@@ -0,0 +1,80 @@
+const chalk = require('chalk');
+
+let prefix = [
+  'painful',
+  '12 hour',
+  'acute',
+]
+
+let adjectives = [
+  'targeted',
+  'precision',
+  'risky',
+]
+
+let specific = [
+  'radiation',
+  'laser',
+  'herbal',
+  'facial',
+]
+
+let nouns = [
+  'chemo',
+  'stem-cell',
+  'expansion',
+  'anxiety',
+  'yoga',
+  'brain',
+  'eye',
+]
+let suffix = [
+  'surgery',
+  'Acupuncture',
+  'treatment',
+  'therapy',
+  'transplants',
+  'exercises',
+]
+
+function getRandom (list, chanceOfEmpty = 1,) {
+  return (chanceOfEmpty === 1 || Math.random() <= chanceOfEmpty) ? list[Math.floor(Math.random() * list.length)] : ""
+}
+
+function generateRouteName () {
+  const names = [
+    getRandom(prefix, 0.44),
+    ' ',
+    getRandom(adjectives, 0.44),
+    ' ',
+    getRandom(specific, 0.44),
+    getRandom(' '),
+    getRandom(nouns),
+    getRandom('-'),
+    getRandom(suffix),
+  ]
+
+  const name = names
+    .filter(v => !!v)
+    .map(str => str.charAt(0).toUpperCase() + str.slice(1))
+    .join('')
+    .replace(/-\s/g, '-')
+    .replace(/\s+/g, ' ')
+    .trim()
+
+  return name
+}
+
+
+if (!module.parent) {
+  const disease = require('./disease.js')
+
+  const d = disease.generateRouteName()
+  const n = generateRouteName()
+
+  console.log('  Mr. Joe,')
+  console.log(`  You've been diagnosed with ${chalk.greenBright(d)}`)
+  console.log('  The doctor is preparing your ' + chalk.red(`${n}`))
+}
+
+module.exports = generateRouteName
